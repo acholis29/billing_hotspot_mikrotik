da73b49031a9558b0fb363780222c85d5fd32b56
acholis29 <diptasoft@gmail.com>
2017-10-29 20:37:41 +0800

    v1.2

--- view/_layout.php
+++ view/_layout.php
@@ -30,8 +30,8 @@
     <link rel="icon" href="../images/siakad.ico" type="image/x-icon">
 
     <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
+    <link href="/css/cyrillic.css" rel="stylesheet" type="text/css">
+    <link href="/css/iconmaterial.css" rel="stylesheet" type="text/css">
 
     <!-- Bootstrap Core Css -->
     <link href="/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
